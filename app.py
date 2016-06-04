7f9a4ed571159f42b5f1414d50e23cfda0bc1403
Luminita <luminita.lh@gmail.com>
2016-06-04 15:55:11 +0300

    settings

--- app.py
+++ app.py
@@ -7,11 +7,13 @@ app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+print(os.environ['APP_SETTINGS'])
+
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-	print(os.environ['APP_SETTINGS'])
     app.run()
 
