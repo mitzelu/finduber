c5271d8471e3cb3ef9f37b4697684c89bb4d74d7
Luminita <luminita.lh@gmail.com>
2016-06-04 17:40:53 +0300

    categories-extract

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 import os
-from flask import Flask, render_template
+import requests
+import operator
+import re
+import nltk
+from flask import Flask, render_template, request
 from flask.ext.sqlalchemy import SQLAlchemy
+from stop_words import stops
+from collections import Counter
+from bs4 import BeautifulSoup
+import urllib2
+
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
@@ -8,11 +17,47 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
-
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    errors = []
+    results = {}
+    if request.method == "POST":
+        # get url that the person has entered
+        try:
+            product = request.form['url']
+            url  = 'http://en.wikipedia.org/wiki/' + product
+            r = requests.get(url)
+        except:
+            errors.append(
+                "Unable to get URL. Please make sure it's valid and try again."
+            )
+            return render_template('index.html', errors=errors)
+        if r:
+            # text processing
+            page = urllib2.urlopen(url)
+            soup = BeautifulSoup(page, 'html5lib')
+            data = soup.find('div', {'class' : 'mw-normal-catlinks'})
+            data = data.findAll('a')
+            categories = []
+
+            for category in data:
+                categories.append((category.text).encode('ascii'))
+
+            # save the results
+            results = categories
+            print results
+            try:
+                result = Result(
+                    url=url,
+                    result_all=raw_word_count,
+                    result_no_stop_words=no_stop_words_count
+                )
+                db.session.add(result)
+                db.session.commit()
+            except:
+                errors.append("Unable to add item to database.")
+    return render_template('index.html', errors=errors, results=results)
+
 
 if __name__ == '__main__':
     app.run()
-
